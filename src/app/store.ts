80d0a4015370a4bdf4d87d1be907831ec5e2de2d
rafael.monteiro <rafael.monteiro@segfy.com>
2021-08-19 08:15:34 -0300

    Search breeds and get images

---
+++ src/app/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/dogs/dogs-api-slice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootsState = ReturnType<typeof store.getState>;
---
+++ src/features/dogs/dogs-api-slice.ts
@@ -0,0 +1,58 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const DOGS_API_KEY =
+  process.env.REACT_APP_DOGS_API_KEY || "REACT_APP_DOGS_API_KEY is missing";
+
+const DOGS_API_BASE_URL = "https://api.thedogapi.com/v1";
+
+interface Breed {
+  id: string;
+  name: string;
+  image: {
+    url: string;
+  };
+}
+
+interface BreedSearch {
+  id: string;
+  name: string;
+  reference_image_id: string;
+}
+
+interface BreedImage {
+  id: string;
+  height: number;
+  width: number;
+  url: string;
+}
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: DOGS_API_BASE_URL,
+    prepareHeaders(headers) {
+      headers.set("x-api-key", DOGS_API_KEY);
+      return headers;
+    },
+  }),
+  endpoints(builder) {
+    return {
+      fetchBreeds: builder.query<Breed[], number | void>({
+        query(limit = 10) {
+          return `/breeds?limit=${limit}`;
+        },
+      }),
+      searchBreeds: builder.query<BreedSearch[], string>({
+        query(query = "") {
+          return `/breeds/search?q=${query}`;
+        },
+      }),
+      getBreedImage: builder.query<BreedImage, string>({
+        query(id) {
+          return `/images/${id}`;
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchBreedsQuery } = apiSlice;
